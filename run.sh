fbee2a2e3d31adcf8ee2d998d4be4a063359a75e
Denys Silveira (Dyonix) <ddbs@icomp.ufam.edu.br>
2014-12-20 20:12:28 -0400

    Versão Final

--- run.sh
+++ run.sh
@@ -2,5 +2,7 @@
 
 rm *.java
 java -jar ../Coco.jar -frames .. forth2c_final_attr.atg
-javac *.class
-java Compile teste.txt > teste.c
+javac *.java
+java Compile blackjack.lf
+gcc codigo_convertido.c -o codigo_convertido -lm
+./codigo_convertido
